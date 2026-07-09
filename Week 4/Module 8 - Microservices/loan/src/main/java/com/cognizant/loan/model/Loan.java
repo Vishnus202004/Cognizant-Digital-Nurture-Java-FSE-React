e26347939761e93c70dddfd3197608be43dc0801
Vishnu S <vishus202004@gmail.com>
2026-07-09 23:47:57 +0530

    feat: add complete week 4 account microservice source files

---
+++ Week 4/Module 8 - Microservices/loan/src/main/java/com/cognizant/loan/model/Loan.java
@@ -0,0 +1,17 @@
+package com.cognizant.loan.model;
+
+public class Loan {
+    private String number;
+    private String type;
+    private double loan;
+    private     private     private     private     private     private     private     private     private     private     private     private     private     private   th    private     private     .loan = loan;
+        this.emi = emi;
+        this.tenure = tenure;
+    }
+
+    public String getNumber() { return number; }
+    public String getType() { return type; }
+    public double getLoan() { return loan; }
+    public double     pu() { return emi; }
+    public int getTenure() { return tenure; }
+}
